1ed8f6810e78e2cd67a611be4e92b5dd1a3ea989
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-03-11 07:46:21 +0100

    updated google.py
    moved google client secret to env

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY')
+app.secret_key = os.environ.get('SECRET_KEY') or "dont guess me"
 
 
 @app.route('/')
--- app/google.py
+++ app/google.py
@@ -1,24 +1,25 @@
 from flask import Flask, redirect, request, session, jsonify
 from requests_oauthlib import OAuth2Session
 from app import app
+import os
 
 # Google OAuth configuration
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 # REDIRECT_URI = 'http://localhost:5000/callback'  # Set this to your callback URL
-REDIRECT_URI = 'https://test-auth-i2mu.onrender.com/google/callback'
+GOOGLE_REDIRECT_URI = 'https://test-auth-i2mu.onrender.com/google/callback'
 
 # Google OAuth endpoints
-AUTHORIZATION_BASE_URL = 'https://accounts.google.com/o/oauth2/auth'
-TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
-USER_INFO_URL = 'https://www.googleapis.com/oauth2/v1/userinfo'
+GOOGLE_AUTHORIZATION_BASE_URL = 'https://accounts.google.com/o/oauth2/auth'
+GOOGLE_TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
+GOOGLE_USER_INFO_URL = 'https://www.googleapis.com/oauth2/v1/userinfo'
 
 
 
 @app.route('/login/google')
 def google_login():
-    google = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=['profile', 'email'])
-    authorization_url, state = google.authorization_url(AUTHORIZATION_BASE_URL, access_type='offline')
+    google = OAuth2Session(GOOGLE_CLIENT_ID, redirect_uri=GOOGLE_REDIRECT_URI, scope=['profile', 'email'])
+    authorization_url, state = google.authorization_url(GOOGLE_AUTHORIZATION_BASE_URL, access_type='offline')
 
     # Save the state to compare it in the callback
     session['oauth_state'] = state
@@ -31,11 +32,11 @@ def google_callback():
     # if request.args.get('state') != session.pop('oauth_state', None):
     #     return 'Invalid state'
 
-    google = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI)
-    token = google.fetch_token(TOKEN_URL, client_secret=CLIENT_SECRET, authorization_response=request.url)
+    google = OAuth2Session(GOOGLE_CLIENT_ID, redirect_uri=GOOGLE_REDIRECT_URI)
+    token = google.fetch_token(GOOGLE_TOKEN_URL, client_secret=GOOGLE_CLIENT_SECRET, authorization_response=request.url)
 
     # Use the token to make a request to the Google API to get user data
-    response = google.get(USER_INFO_URL)
+    response = google.get(GOOGLE_USER_INFO_URL)
 
     if response.status_code == 200:
         user_data = response.json()
